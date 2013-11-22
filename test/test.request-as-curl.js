5614d271cc7519c75690efd8257e2c1317ebbbdb
Mikhail Davydov <azazel.private@gmail.com>
2013-11-23 00:19:25 +0600

    +tests; coverage 100%

--- test/test.request-as-curl.js
+++ test/test.request-as-curl.js
@@ -1,5 +1,5 @@
 /*global describe, it, beforeEach, afterEach*/
-/*jshint expr:true*/
+/*jshint expr:true, maxstatements:30 */
 
 var serializeRequest = require('..'),
     express = require('express'),
@@ -12,8 +12,7 @@ var serializeRequest = require('..'),
 var PORT = 54321;
 var HOSTNAME = 'localhost';
 var PATH = '/p/a/t/h?query=string';
-var METHOD = 'POST';
-var BODY = 'data\ndata\ndata';
+var BODY = 'data\ndata\ndata\nдата-дада●★☆♣♥✌\x01\x17';
 var BODY_JSON = {
     pewpew: {
         ololo: []
@@ -45,6 +44,8 @@ var httpRequest = function (options, content, cb) {
 
 describe('request-as-curl', function () {
     var app, server;
+    var oldPlatform = process.platform;
+
     beforeEach(function(done) {
         app = express();
         app.all('*', function (req, res) {
@@ -59,11 +60,17 @@ describe('request-as-curl', function () {
         server = app.listen(PORT, function () {
             done();
         });
+        process.platform === oldPlatform;
+        if (process.env.REQUEST_AS_CURL_COVERAGE) {
+            delete require.cache[require.resolve('../lib-cov/escapeCliCommand')];
+        } else {
+            delete require.cache[require.resolve('../lib/escapeCliCommand')];
+        }
     });
 
     afterEach(function (done) {
         server.close();
-        setTimeout(done, 0);
+        setTimeout(done, 20);
     });
 
     it('should be able to serialize http.request', function (done) {
@@ -71,7 +78,7 @@ describe('request-as-curl', function () {
             hostname: HOSTNAME,
             port: PORT,
             path: PATH,
-            method: METHOD
+            method: 'POST'
         },
         BODY,
         function (error, response, expected) {
@@ -80,10 +87,73 @@ describe('request-as-curl', function () {
         });
     });
 
+    it('should be able to serialize http.request with empty body', function (done) {
+        var req = httpRequest({
+                hostname: HOSTNAME,
+                port: PORT,
+                path: PATH,
+                method: 'GET'
+            },
+            null,
+            function (error, response, expected) {
+                expect(serializeRequest(req)).to.eql(expected);
+                done();
+            });
+    });
+
+    it('should be able to serialize PUT http.request', function (done) {
+        var req = httpRequest({
+                hostname: HOSTNAME,
+                port: PORT,
+                path: PATH,
+                method: 'PUT'
+            },
+            null,
+            function (error, response, expected) {
+                expect(serializeRequest(req)).to.eql(expected);
+                done();
+            });
+    });
+
+    it('should be able to serialize empty request', function () {
+        expect(serializeRequest()).to.eql('curl \'http://localhost\' --compressed');
+    });
+
+    it('should be able to serialize https request', function () {
+        expect(serializeRequest({
+            headers: {
+                host: 'localhost:443'
+            }
+        })).to.eql('curl \'https://localhost:443\' --compressed');
+    });
+
+    it('should be able to serialize broken ClientRequest', function (done) {
+        var req = new http.get('http://' + HOSTNAME + ':' + PORT + PATH, function () {
+            delete req._header;
+            expect(serializeRequest(req)).to.eql('curl \'http://localhost:54321/p/a/t/h?query=string\' --compressed');
+            done();
+        });
+    });
+
+    it('should be able to escape query for win32', function (done) {
+        process.platform = 'win32';
+        var req = httpRequest({
+                hostname: HOSTNAME,
+                port: PORT,
+                path: PATH,
+                method: 'POST'
+            },
+            BODY,
+            function (error, response, expected) {
+                expect(serializeRequest(req, BODY)).to.eql(expected);
+                done();
+            });
+    });
+
     it('should be able to serialize npm request module', function (done) {
         var req = request('http://' + HOSTNAME + ':' + PORT + PATH,
         {
-            method: METHOD,
+            method: 'POST',
             body: BODY
         },
         function (error, response, expected) {
@@ -95,7 +165,7 @@ describe('request-as-curl', function () {
     it('should be able to serialize npm request module with data json', function (done) {
         var req = request('http://' + HOSTNAME + ':' + PORT + PATH,
         {
-            method: METHOD,
+            method: 'POST',
             json: BODY_JSON
         },
         function (error, response, expected) {
